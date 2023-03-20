4633a2e7ad365c9df79a50f9a25abf9a8dd2c29c
Stef Altavista <stefanomsc@hotmail.it>
2023-03-20 21:01:51 +0100

    pages Link

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,13 +3,19 @@ import { Routes, Route } from "react-router-dom";
 
 import Header from "./Header";
 import Home from "./Home";
+import Archive from "./Archive";
+import About from "./About";
+import Dashboard from "./Dashboard";
 
 export default function App() {
     return (
         <div id="main">
             <Header></Header>
             <Routes>
-                <Route path="/" element={<Home></Home>}></Route>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/archive" element={<Archive />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/dashboard" element={<Dashboard />}></Route>
             </Routes>
         </div>
     );
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../../css/header.css";
+import Menu from "./Menu";
 export default function Header() {
     return (
         <div id="header">
             <img src="/images/logoAutonoma.png" />
-            <p>Welcome to Autonoma industriale</p>
+
+            <Menu />
         </div>
     );
 }
